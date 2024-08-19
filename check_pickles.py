a831f13f90a9f64a79174be869f6c780308d357f
Ruitang Yang <ruitang.yang@geo.uio.no>
2024-08-19 10:49:03 +0200

    Removed check_pickles.py as it's no longer needed

--- check_pickles.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-with open('/home/ruitang/Astra_Ruitang_UIO/PyGEM_TEST_Simple/Advanced_test/oggm_gdirs/per_glacier/RGI60-15/RGI60-15.03/RGI60-15.03733/downstream_line.pkl', 'rb') as file:
-    # Load the object from the file
-    data = pickle.load(file)
