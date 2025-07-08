aa3c9a7a0acbebc4cb7e45aaa452e9907ae1b806
Ruitang Yang <ruitang.yang@geo.uio.no>
2025-07-08 16:18:16 +0200

    add the command example; adjust the task_id range

--- run_calibration_AMIS_Region.sh
+++ run_calibration_AMIS_Region.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop over task files from Task0.txt to TaskN.txt
-for task_id in {0..11}  # Change 9 to however many tasks you have
+for task_id in {0..11}  # Change 9 to however many tasks you have {0..11}
 do
     echo "Running task $task_id..."
 
@@ -16,3 +16,19 @@ do
         -Visualize_Index \
         -v -debug
 done
+
+# command example to run the sinle task
+# Uncomment the following line to run a single task without the loop
+# Note: Make sure to replace the task_id with the desired task number (0, 1, 2, ..., N)
+# This command is equivalent to running the script for task 0.
+# Make sure to adjust the paths and filenames as necessary for your environment.
+# Example command to run a single task:
+# python -u run_calibration_AMIS_MB_FA_20002010_Parallel_Log_New.py \
+# -rgi_region01 07 -ref_startyear 2000 -ref_endyear 2019 \
+# -frontalablation_fn "frontal_ablation_obs_20002010_task0.csv" \
+# -hugonnet_fn "mass_balance_obs_20002010_task0.csv" \
+# -lengthchange_annual_fn "lengthchange_annual_rgi_region01_7_20002020_task0.csv" \
+#-store_monthly_step -Visualize_Index -v -debug
+
+# Example command to run a single task with specific parameters: ONELINE
+# python -u run_calibration_AMIS_MB_FA_20002010_Parallel_Log_New.py -rgi_region01 07 -ref_startyear 2000 -ref_endyear 2019 -frontalablation_fn "frontal_ablation_obs_20002010_task0.csv" -hugonnet_fn "mass_balance_obs_20002010_task0.csv" -lengthchange_annual_fn "lengthchange_annual_rgi_region01_7_20002020_task0.csv" -store_monthly_step -Visualize_Index -v -debug
