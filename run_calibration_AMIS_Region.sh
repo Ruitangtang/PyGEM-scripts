8268eab2218b3d8854a0a5271ddb195fc3c931a8
Ruitang Yang <ruitang.yang@geo.uio.no>
2026-02-20 18:28:01 +0100

    update the calibration script's name

--- run_calibration_AMIS_Region.sh
+++ run_calibration_AMIS_Region.sh
@@ -5,7 +5,7 @@ for task_id in {0..11}  # Change 9 to however many tasks you have {0..11}
 do
     echo "Running task $task_id..."
 
-    python -u run_calibration_AMIS_MB_FA_20002010_Parallel_Log_New.py \
+    python -u run_calibration_AMIS_SERMeQ.py \
         -rgi_region01 07 \
         -ref_startyear 2000 \
         -ref_endyear 2019 \
@@ -23,7 +23,7 @@ done
 # This command is equivalent to running the script for task 0.
 # Make sure to adjust the paths and filenames as necessary for your environment.
 # Example command to run a single task:
-# python -u run_calibration_AMIS_MB_FA_20002010_Parallel_Log_New.py \
+# python -u run_calibration_AMIS_SERMeQ.py \
 # -rgi_region01 07 -ref_startyear 2000 -ref_endyear 2019 \
 # -frontalablation_fn "frontal_ablation_obs_20002010_task0.csv" \
 # -hugonnet_fn "mass_balance_obs_20002010_task0.csv" \
@@ -31,4 +31,6 @@ done
 #-store_monthly_step -Visualize_Index -v -debug
 
 # Example command to run a single task with specific parameters: ONELINE
-# python -u run_calibration_AMIS_MB_FA_20002010_Parallel_Log_New.py -rgi_region01 07 -ref_startyear 2000 -ref_endyear 2019 -frontalablation_fn "frontal_ablation_obs_20002010_task0.csv" -hugonnet_fn "mass_balance_obs_20002010_task0.csv" -lengthchange_annual_fn "lengthchange_annual_rgi_region01_7_20002020_task0.csv" -store_monthly_step -Visualize_Index -v -debug
+# python -u run_calibration_AMIS_SERMeQ.py -rgi_region01 07 -ref_startyear 2000 -ref_endyear 2019 -frontalablation_fn "frontal_ablation_obs_20002010_task${task_id}.csv" -hugonnet_fn "mass_balance_obs_20002010_task${task_id}.csv" -lengthchange_annual_fn "lengthchange_annual_rgi_region01_7_20002020_task${task_id}.csv" -store_monthly_step -Visualize_Index -v -debug
+# Example command to run single glaciers with specific parameters: ONELINE
+#  python -u run_calibration_AMIS_SERMeQ.py -rgi_region01 07 -rgi_glac_number '7.01506' -ref_startyear 2000 -ref_endyear 2019 -frontalablation_fn "frontal_ablation_obs_20002010.csv" -hugonnet_fn "mass_balance_obs_20002010.csv" -lengthchange_annual_fn "lengthchange_annual_rgi_region01_7_20002020.csv" -store_monthly_step -Visualize_Index -v -debug
